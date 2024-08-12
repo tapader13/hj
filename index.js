6a5ac1fa4e080868f7ae8b396efe942992bd5f9e
tapader13 <minhajtapader0@gmail.com>
2024-08-12 13:15:03 -0400

    try domainin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5175/',
+    origin: 'https://hj-client.vercel.app/',
     exposedHeaders: ['total'],
   })
 );
