66e27a8b30a1a82cd7ee1b4f6a1109fa470d3b9f
tapader13 <minhajtapader0@gmail.com>
2024-08-12 13:19:21 -0400

    final try

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://hj-client.vercel.app/',
+    origin: 'https://hj-client.vercel.app/api/',
     exposedHeaders: ['total'],
   })
 );
