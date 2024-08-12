cb88cad87a8d0d9cac93de0ef62e7dbe0195f061
tapader13 <minhajtapader0@gmail.com>
2024-08-12 12:49:51 -0400

    try

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://jobportal-client-v32s.vercel.app/',
+    origin: 'http://localhost:5175/',
     exposedHeaders: ['total'],
   })
 );
